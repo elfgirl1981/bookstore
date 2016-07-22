a05c7c5bf8b917129f508b06eb3f74363e108384
Amanda <elfgirl1981@yahoo.com>
2016-07-21 22:53:53 -0500

    Adds Admin management functions

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,33 +6,45 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-      if @author.save
-        flash[:notice] = 'Author created'
+    if @author.save
+      flash[:notice] = "Author Created"
 
-        redirect_to authors_path
-      else
-        render 'new'
-      end
+      redirect_to authors_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @author = Author.find(params[:id])
+
+    @author.update(author_params)
+
+    flash[:notice] = 'Author Updated'
+
+    redirect_to authors_path
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
   def destroy
+    @author = Author.find(params[:id])
+
+    @author.destroy
+
+    flash[:notice] = 'Author Removed'
+
+    redirect_to authors_path
   end
 
   def index
-  end
-
-  def show
+    @authors = Author.all
   end
 
   private
     def author_params
       params.require(:author).permit(:first_name, :last_name)
-  end
-
+    end
 end
